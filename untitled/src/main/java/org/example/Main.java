a31cbf9de6e6d31631de9c7683a3b6fb007787c1
manmat <manmat2004@gmail.com>
2025-03-19 17:25:17 +0200

    am merguit totul, Client si Utilizator e fara IA, Produs si Cos e cu IA

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -4,18 +4,36 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+            Produs ciocolata = new Produs(1, "Ciocolată Milka", 7.5f,
+                    "Ciocolată gustoasă cu lapte cremos si alune de pădure", 50);
+            Produs liptonPiersici = new Produs(2, "Lipton Piersici", 5.0f,
+                    "Ceai răcoritor cu aromă de piersici", 100);
+
+            ciocolata.actualizeazaStoc(-20);
+            liptonPiersici.aplicaReducere(50);
+
+            ciocolata.afiseazaDetalii();
+            liptonPiersici.afiseazaDetalii();
+
+            Cos cos = new Cos();
+            cos.adaugaProdus(ciocolata, 10);
+            cos.adaugaProdus(liptonPiersici);
+
+            cos.afiseazaProduse();
+            float total = cos.calculeazaTotal();
+            System.out.println("Total: " + total + " RON");
 
             Client u1 = new Client(123,"Matei", "manmat2004@gmail.com", "asd123", "Str. Copou, nr14", "0724569595");
-            Produs produs = new Produs(452);
+
             Comanda comanda = new Comanda(1);
             u1.CreeazaCont();
             u1.Autentificare("manmat2004@gmail.com", "asd123");
             u1.cautaProduse("MacBook Pro");
-            u1.adaugaInCos(produs);
-            u1.stergeDinCos(produs);
+            u1.adaugaInCos(ciocolata);
+            u1.stergeDinCos(ciocolata);
             u1.finalizeazaComanda(comanda);
-            u1.adaugaRecenzie(produs, "Foarte bun, s-a stricat in prima zi", 2);
-            u1.stergeRecenzie(produs);
+            u1.adaugaRecenzie(ciocolata, "Foarte bun, s-a stricat in prima zi", 2);
+            u1.stergeRecenzie(ciocolata);
             u1.platesteInRate();
             u1.plateste();
 
--- untitled/src/main/java/org/example/Produs.java
+++ untitled/src/main/java/org/example/Produs.java
@@ -1,15 +1,37 @@
 package org.example;
+import lombok.*;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Produs {
     private int id;
-    Produs(int id) {
-        this.id = id;
+    private String nume;
+    private float pret;
+    private String descriere;
+    private int stoc;
+
+    public void afiseazaDetalii() {
+        System.out.println(this);
     }
 
-    @Override
-    public String toString() {
-        return "Produs{" +
-                "id=" + id +
-                '}';
+    public void actualizeazaStoc(int cantitate) {
+        if (cantitate < 0 && Math.abs(cantitate) > stoc) {
+            System.out.println("Stoc insuficient!");
+        } else {
+            stoc += cantitate;
+            System.out.println("Stoc actualizat: " + stoc);
+        }
+    }
+
+    public void aplicaReducere(float procent) {
+        if (procent > 0 && procent <= 100) {
+            pret -= pret * (procent / 100);
+            System.out.println("Preț redus la: " + pret + " RON");
+        } else {
+            System.out.println("Reducere invalidă!");
+        }
     }
 }
